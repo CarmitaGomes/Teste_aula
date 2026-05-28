c5ff9da2a059e03836d0e1d28bf270e0715b990d
Carmita <125024649+CarmitaGomes@users.noreply.github.com>
2026-05-28 13:40:19 +0100

    commit_1
    Adicionada a pergunta de moradia

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+print(f"\nOlá, {nome}!")
 idade: int = int(input("Qual é a sua idade? "))
 print(f"Tu tens {idade} anos...")
+
+cidade: str = input("\nEm que cidade vives? ")
+print(f"Bom saber que vives em {cidade}!")
 
