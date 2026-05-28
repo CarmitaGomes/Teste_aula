8f6006827075d30e2133469a99077ebdfb01324a
Carmita <125024649+CarmitaGomes@users.noreply.github.com>
2026-05-28 13:48:12 +0100

    commit_4
    Adicionadas condições para print de textos

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ print(f"\nOlá, {nome}!")
 idade: int = int(input("Qual é a sua idade? "))
 print(f"Tu tens {idade} anos...")
 
+if idade < 18:
+    print("Tu ainda és menor de idade!")
+else:
+    print("Tu já és maior de idade!")
+
 cidade: str = input("\nEm que cidade vives? ")
 pais: str = input("Em que país fica essa cidade? ")
 
